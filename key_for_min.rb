229e63a37f98b2e9c8334673e5febb580dff5e73
Kristen Robertson <kristen77@gmail.com>
2018-10-02 11:42:02 +0000

    automatically backed up by learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -9,6 +9,8 @@ def key_for_min_value(name_hash)
         min_key = key
         min_value = value
         elsif value < min_value
+        min_key = key
+        min_value = value
     end
   end
   min_key
